6b6c82810f65c99cabb1932cf420bcc3f2513e5f
Matvey Kolesov <kolesov645@gmail.com>
2024-02-07 11:04:23 +0300

    Add debug asserts and result array initialization to compare_op

--- src/compare_op.c
+++ src/compare_op.c
@@ -1,3 +1,15 @@
+/**
+ * @file compare_op.c
+ * @author Matvey Kolesov (kolesov645@gmail.com)
+ * @brief Compares complexity of "+" and "*" operations in field GF(65536).
+ * @version 0.1
+ * @date 2024-02-07
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <assert.h>
 #include <immintrin.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -20,13 +32,20 @@ void compare_by_clock(const GF_t *gf, const element_t *a, const element_t *b,
         __m256 *a256 = (__m256 *)a;
         __m256 *b256 = (__m256 *)b;
         __m256 *c256 = (__m256 *)c;
-        size_t iter_cnt = NUMBER_OF_OPERATIONS * sizeof(element_t) / sizeof(__m256);
+        size_t iter_cnt =
+            NUMBER_OF_OPERATIONS * sizeof(element_t) / sizeof(__m256);
 
         start = clock();
         for (size_t i = 0; i < iter_cnt; ++i) {
             c256[i] = _mm256_xor_ps(a256[i], b256[i]);
         }
         end = clock();
+
+#ifndef NDEBUG
+        for (size_t i = 0; i < NUMBER_OF_OPERATIONS; ++i) {
+            assert(c[i] == a[i] ^ b[i]);
+        }
+#endif
     } else {
         start = clock();
         for (size_t i = 0; i < NUMBER_OF_OPERATIONS; ++i) {
@@ -48,7 +67,8 @@ void compare_by_clock(const GF_t *gf, const element_t *a, const element_t *b,
     printf("===== clock() =====\n");
     printf("    ^     time: %.3f secs\n", elapsed_time_add);
     printf("gf_mul_ee time: %.3f secs\n", elapsed_time_mul);
-    printf("ratio \"gf_mul_ee/^\": %.3f\n", elapsed_time_mul / elapsed_time_add);
+    printf("ratio \"gf_mul_ee/^\": %.3f\n",
+           elapsed_time_mul / elapsed_time_add);
 }
 
 int main(void) {
@@ -67,14 +87,16 @@ int main(void) {
 
     gf_init(gf);
 
-    a = (element_t *)aligned_alloc(32, NUMBER_OF_OPERATIONS * sizeof(element_t));
+    a = (element_t *)aligned_alloc(32,
+                                   NUMBER_OF_OPERATIONS * sizeof(element_t));
     if (!a) {
         printf("ERROR: couldn't allocate a\n");
         gf_free(gf);
         return 1;
     }
 
-    b = (element_t *)aligned_alloc(32, NUMBER_OF_OPERATIONS * sizeof(element_t));
+    b = (element_t *)aligned_alloc(32,
+                                   NUMBER_OF_OPERATIONS * sizeof(element_t));
     if (!b) {
         printf("ERROR: couldn't allocate b\n");
         free(a);
@@ -82,7 +104,8 @@ int main(void) {
         return 1;
     }
 
-    c = (element_t *)aligned_alloc(32, NUMBER_OF_OPERATIONS * sizeof(element_t));
+    c = (element_t *)aligned_alloc(32,
+                                   NUMBER_OF_OPERATIONS * sizeof(element_t));
     if (!c) {
         printf("ERROR: couldn't allocate c\n");
         free(b);
@@ -96,6 +119,7 @@ int main(void) {
     for (size_t i = 1; i < NUMBER_OF_OPERATIONS; ++i) {
         a[i] = a[i - 1] + 1;
         b[i] = b[i - 1] + 1;
+        c[i] = 0;
     }
 
     compare_by_clock(gf, a, b, c);
